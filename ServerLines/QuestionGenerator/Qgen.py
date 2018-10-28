4f2079c50d8b4b95a7dfc1138dd1e8d58a7dc5b1
Vishal Gupta <vishalg8897@gmail.com>
2018-10-28 06:39:37 +0530

    Added timecop

--- ServerLines/QuestionGenerator/Qgen.py
+++ ServerLines/QuestionGenerator/Qgen.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import spacy
 import random
+import time
 from nltk import word_tokenize,sent_tokenize
 # from nltk.util import ngrams
 from gensim.models import Word2Vec
@@ -19,6 +20,8 @@ TEST_TEXT = """
 The Battle of Plassey was a decisive victory of the British East India Company over the Nawab of Bengal and his French allies on 23 June 1757. The battle consolidated the Company's presence in Bengal, which later expanded to cover much of India over the next hundred years. The battle took place at Palashi (Anglicised version: Plassey) on the banks of the Hooghly River, about 150 kilometres (93 mi) north of Calcutta and south of Murshidabad, then capital of Bengal (now in Nadia district in West Bengal). The belligerents were the Nawab Sirajuddaulah, the last independent Nawab of Bengal, and the British East India Company. Siraj-ud-daulah had become the Nawab of Bengal the year before, and he ordered the English to stop the extension of their fortification. Robert Clive bribed Mir Jafar, the commander in chief of the Nawab's army, and also promised him to make him Nawab of Bengal. He defeated the Nawab at Plassey in 1757 and captured Calcutta. The battle was preceded by the attack on British-controlled Calcutta by Nawab Siraj-ud-daulah and the Black Hole massacre. The British sent reinforcements under Colonel Robert Clive and Admiral Charles Watson from Madras to Bengal and recaptured Calcutta. Clive then seized the initiative to capture the French fort of Chandernagar. Tensions and suspicions between Siraj-ud-daulah and the British culminated in the Battle of Plassey. The battle was waged during the Seven Years' War (1756–1763), and, in a mirror of their European rivalry, the French East India Company (La Compagnie des Indes Orientales) sent a small contingent to fight against the British. Siraj-ud-Daulah had a numerically superior force and made his stand at Plassey. The British, worried about being outnumbered, formed a conspiracy with Siraj-ud-Daulah's demoted army chief Mir Jafar, along with others such as Yar Lutuf Khan, Jagat Seths (Mahtab Chand and Swarup Chand), Omichund and Rai Durlabh. Mir Jafar, Rai Durlabh and Yar Lutuf Khan thus assembled their troops near the battlefield but made no move to actually join the battle. Siraj-ud-Daulah's army with 50,000 soldiers, 40 cannons and 10 war elephants was defeated by 3,000 soldiers of Col. Robert Clive, owing to the flight of Siraj-ud-daulah from the battlefield and the inactivity of the conspirators. The battle ended in 11 hours. This is judged to be one of the pivotal battles in the control of Indian subcontinent by the colonial powers. The British now wielded enormous influence over the Nawab and consequently acquired significant concessions for previous losses and revenue from trade. The British further used this revenue to increase their military might and push the other European colonial powers such as the Dutch and the French out of South Asia, thus expanding the British Empire.
 #""".strip()
 
+def timecop(tag,start):
+    print (tag.upper()[:3]+"_TIME:"+time.time()-start)
 
 ENTITY_PRIORITIES = {
     "PERSON": 20,
@@ -440,16 +443,21 @@ def gen_sents(doc,limit=15,largeDoc = None):
         :param doc: Spacy Doc object, Used to create questions
         :param limit=20: Upper Limit on number of questions to be returned
     """
+
+    start = time.time()
     ents = get_entities(doc)
+    timecop("entities",start)
     if largeDoc is None:
         w2v_model = gen_word2vec(doc)
         large_type2ent = None
     else:
         large_ents = get_entities(largeDoc)
         w2v_model = gen_word2vec(largeDoc)
-        large_type2ent = map_ents_to_types_only(large_ents, largeDoc)
-
-    ent2type, type2ent, counter, sent2ent = map_ents_to_types(ents, doc)
+        large_type2ent = map_ents_to_types_only(large_ents, largeDoc)    
+    timecop("Large",start)
+    
+    ent2type, type2ent, counter, sent2ent = map_ents_to_types(ents, doc)    
+    timecop("doc",start)
 
     if largeDoc is None:
         large_type2ent = type2ent
@@ -490,7 +498,7 @@ def gen_sents(doc,limit=15,largeDoc = None):
                 QUESTION_RANK: Q_TYPE_ENT + str(len(result))
             }
             result.append(sample)
-    
+    timecop("generate ents",start)
     # Sort by entity type, choose top 20 and then shuffle.
     verb_sents = set()
     verb_qs = set()
@@ -499,12 +507,13 @@ def gen_sents(doc,limit=15,largeDoc = None):
     if len(result)<limit:
         verb_qs,verb_sents = get_verb_qs(doc , ent_sents)
         result += random.sample(verb_qs, min(limit - len(result), len(verb_qs)))
-    
+    timecop("VERB",start)
     print ("EV  :",len(result))
     if len(result) < limit:
         noun_qs,_ = get_noun_sents(doc,verb_sents.union(ent_sents))
         result += random.sample(noun_qs , min(limit - len(result) , len(noun_qs)))
-
+    
+    timecop("NOUN",start)    
     print ("ENV :",len(result))
 
 
